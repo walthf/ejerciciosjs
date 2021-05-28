4c4c8d84ccd05219ce0285d57cb579da5f4cd09e
walthf <walterhf@gmail.com>
2021-05-27 17:51:33 -0700

    1 file modified

--- index.js
+++ index.js
@@ -119,8 +119,9 @@
 
 // console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`)
 
-
 const bill = 430;
-const tip = bill >=50 && bill <=300 ? bill * .15 : bill  * .20;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 const total = bill + tip;
-console.log(`El monto es: $${bill} y la propina $${tip}, dando un total de $${total}`)
+console.log(
+  `El monto es: $${bill} y la propina $${tip}, dando un total de $${total}`
+);
